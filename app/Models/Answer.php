5ca527fa2e05c7d93679d8fa04d55ce501525201
Daryl Ferrer Legion <experth6@gmail.com>
2023-08-20 16:00:28 +0800

    WIP

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\DB;
 
 /**
  * App\Models\Answer
